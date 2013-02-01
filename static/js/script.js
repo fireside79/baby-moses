c4c9650b439b9946b99c3e543ce3cb7a0722cb2d
Jeffrey Stilwell <github@firesideinteractive.com>
2013-02-01 01:28:58 -0600

    Adding static files

---
+++ static/js/script.js
@@ -0,0 +1,26 @@
+$(function(){
+// IPad/IPhone
+	var viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]'),
+    ua = navigator.userAgent,
+ 
+    gestureStart = function () {
+        viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+    },
+ 
+    scaleFix = function () {
+      if (viewportmeta && /iPhone|iPad/.test(ua) && !/Opera Mini/.test(ua)) {
+        viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+        document.addEventListener("gesturestart", gestureStart, false);
+      }
+    };
+scaleFix();
+// Menu Android
+	var userag = navigator.userAgent.toLowerCase();
+	var isAndroid = userag.indexOf("android") > -1; 
+	if(isAndroid) {
+		$('.sf-menu').responsiveMenu({autoArrows:true});
+	}
+	$('.search').hover(function(){$(this).stop().animate({opacity:.5})},(function(){$(this).stop().animate({opacity:1})}))
+	$('.tooltips li a').hover(function(){$(this).find('img').stop().animate({opacity:.5})},(function(){$(this).find('img').stop().animate({opacity:1})}))
+	$('#contact-form').forms({ownerEmail:'#'})
+});
